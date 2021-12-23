cb5f953389dd819d0a8dd690e7028464dab345b3
José Ignacio Escribano <jiep12345@hotmail.com>
2021-12-23 11:44:45 +0100

    Add Classic McEliece 460896 (VEC)

--- src/kem/classic_mceliece/pqclean_mceliece460896_avx/encrypt.h
+++ src/kem/classic_mceliece/pqclean_mceliece460896_avx/encrypt.h
@@ -5,7 +5,6 @@
 */
 
 
-void PQCLEAN_MCELIECE460896_AVX_encrypt(unsigned char * /*s*/, unsigned char * /*e*/, const unsigned char * /*pk*/);
-
+void PQCLEAN_MCELIECE460896_AVX_encrypt(unsigned char * /*s*/, const unsigned char * /*e*/, const unsigned char * /*pk*/);
+void gen_e_460896_avx(unsigned char *e);
 #endif
-
